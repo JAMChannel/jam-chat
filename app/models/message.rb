5e549321ecb0df78d53e7045f68d6b9ead073e21
JAMChannel <55085180+JAMChannel@users.noreply.github.com>
2021-06-26 15:26:28 +0900

    画像機能追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,15 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   validates :content, presence: true
+  has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+  # was_attached?メソッドは、self.image.attached?という記述によって、
+  # 画像があればtrue、なければfalseを返す仕組みです。
+  # これにより、画像が存在しなければテキストが必要となり、
+  # 画像があればテキストは不要になりました。
 end
